6073199975bdaa6a9ee25f20b0b1de3c6aeea1b3
James Henry <james@henry.sc>
2026-09-24 17:16:30 +0400

    polygraph 0.1.5 (#5)

--- Formula/polygraph.rb
+++ Formula/polygraph.rb
@@ -1,22 +1,33 @@
-require "language/node"
-
 class Polygraph < Formula
   desc "CLI to work with Polygraph for agentic cross-repo coordination"
   homepage "https://trypolygraph.com"
-  url "https://registry.npmjs.org/polygraph/-/polygraph-0.1.4.tgz"
-  sha256 "9731324404a4d311995188b31824db79388115b7d170b397d0c809d4f5a5686c"
+  version "0.1.5"
   license :cannot_represent # proprietary; mirrors npm "license": "proprietary"
 
-  livecheck do
-    url :stable
-    strategy :npm
+  on_macos do
+    on_arm do
+      url "https://storage.googleapis.com/nx-public-builds-70m638/polygraph-cli/0.1.5%2B8280e8990a03/polygraph-darwin-arm64"
+      sha256 "0c10d88cd086d48815ba1a4a5bc14ee8820b8dc78e08a2bbaf68ebd03fdbcc3b"
+    end
+    on_intel do
+      url "https://storage.googleapis.com/nx-public-builds-70m638/polygraph-cli/0.1.5%2B8280e8990a03/polygraph-darwin-x64"
+      sha256 "789d86545df55af974487b092358bc5371c43df51c150d21306e4d70b4afcaea"
+    end
   end
 
-  depends_on "node"
+  on_linux do
+    on_arm do
+      url "https://storage.googleapis.com/nx-public-builds-70m638/polygraph-cli/0.1.5%2B8280e8990a03/polygraph-linux-arm64"
+      sha256 "74eb6c2797583e03cdb88658162b237215e9722d067ea9b69e7d6bd1c13b480b"
+    end
+    on_intel do
+      url "https://storage.googleapis.com/nx-public-builds-70m638/polygraph-cli/0.1.5%2B8280e8990a03/polygraph-linux-x64"
+      sha256 "f45b5a6c8c567df8fb8fbb8ab34f1e699db25f9a26bb4054069dc4325c518ec4"
+    end
+  end
 
   def install
-    system "npm", "install", *std_npm_args
-    bin.install_symlink Dir["#{libexec}/bin/*"]
+    bin.install Dir["polygraph-*"].first => "polygraph"
   end
 
   test do
